f0d4c5f495835b10d303affa9dca65c42fd752ec
Divya Kala <121877809+divya123-123@users.noreply.github.com>
2024-05-19 13:45:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from "react";
+import LoginSignup from "./Components/LoginSignUp/LoginSignup";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
